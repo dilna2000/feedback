ec7dcae73bf470672fa4cb56907b5d713c33ffb0
Shivam <ammudilna2000@gmail.com>
2022-03-15 23:49:47 +0530

    first commit

---
+++ form/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from form import views
+from django.urls import path
+from django.contrib import admin
+
+urlpatterns = [
+    path('',views.feedback,name="index"),
+    path('submit_feedback',views.submit_feedback,name="submit_feedback")
+
+]
---
+++ form/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from form.models import Feedback
+
+# Create your views here.
+def feedback(request):
+    return render(request,'feedback.html')
+def submit_feedback(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        details = request.POST['feedback']
+        feedb = Feedback(customer_name=name,email=email,details=details)
+        feedb.save()
+    res = render(request,'thanks.html')
+    return res
