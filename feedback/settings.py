1cd5f52b4231ce3c13ec512dd9917cf5e27810d0
Shivam <ammudilna2000@gmail.com>
2022-03-23 00:36:40 +0530

    3 commit

--- feedback/settings.py
+++ feedback/settings.py
@@ -127,4 +127,4 @@ STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL = '/images/'
+
